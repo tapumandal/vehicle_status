afbedf3764feb3e9e8face579cf3f57f875b85ae
online.tapu@gmail.com <online.tapu@gmail.com>
2019-06-13 04:19:03 +0600

    Status save in the firebase

--- app/src/main/java/vehicle_status/tapumandal/me/activity/MainActivity.java
+++ app/src/main/java/vehicle_status/tapumandal/me/activity/MainActivity.java
@@ -1,11 +1,12 @@
-package vehicle_status.tapumandal.me;
+package vehicle_status.tapumandal.me.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import vehicle_status.tapumandal.me.R;
 
 public class MainActivity extends AppCompatActivity {
 
